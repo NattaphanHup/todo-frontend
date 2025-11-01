e6cc513cb89a46f68f98a7455ff82be1c6a0ea1f
Nattaphan Buathong <65160069@go.buu.ac.th>
2025-11-01 12:46:15 +0700

    refactor: update App.vue to manage todos with axios and enhance UI

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Todo List</h1>
+    <input
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+      placeholder="Add new todo"
+    />
+    <button @click="addTodo">Add</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <span
+          :class="{ completed: todo.completed }"
+          style="margin-right: 0.6rem"
+          >{{ todo.text }}</span
+        >
+        <button @click="toggleCompleted(todo)">Toggle</button>
+        <button @click="deleteTodo(todo.id)">Delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todos: [],
+      newTodo: "",
+    };
+  },
+  async created() {
+    this.fetchTodos();
+  },
+  methods: {
+    async fetchTodos() {
+      const res = await axios.get(`${process.env.VUE_APP_API_URL}/todos`); // เปลี่ยนเป็น backend URL จริงตอน deploy
+      this.todos = res.data;
+    },
+    async addTodo() {
+      if (this.newTodo) {
+        await axios.post(`${process.env.VUE_APP_API_URL}/todos`, {
+          text: this.newTodo,
+        });
+        this.newTodo = "";
+        this.fetchTodos();
+      }
+    },
+    async toggleCompleted(todo) {
+      console.log(todo);
+      const res = await axios.put(
+        `${process.env.VUE_APP_API_URL}/todos/${todo.id}`,
+        {
+          completed: !todo.completed,
+        }
+      );
+      this.fetchTodos();
+    },
+    async deleteTodo(id) {
+      await axios.delete(`${process.env.VUE_APP_API_URL}/todos/${id}`);
+      this.fetchTodos();
+    },
+  },
+};
 </script>
 
 <style>
+.completed {
+  text-decoration: line-through;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  justify-items: center;
 }
 </style>
